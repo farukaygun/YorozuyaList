215fa4da71cf8694f51046c095a004f6d3c289aa
Faruk Aygun <farukaygun1@outlook.com>
2023-10-14 16:01:50 +0300

    Firebase Dynamic Links changed to Deep Link.
    - Bump version.

--- app/src/main/java/com/farukaygun/yorozuyalist/util/Constants.kt
+++ app/src/main/java/com/farukaygun/yorozuyalist/util/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
 
 	const val BASE_API_URL = "https://api.myanimelist.net/v2/"
 	const val OAUTH2_URL = "https://myanimelist.net/v1/oauth2/"
-	const val YOROZUYA_PAGELINK = "yorozuyalist://yorozuyalist1.page.link"
+	const val YOROZUYA_PAGELINK = "app://com.farukaygun.yorozuyalist"
 
 	const val STATE = "YorozuyaList"
 	const val RESPONSE_TYPE = "code"
